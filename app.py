f991a3479bfc4e11ad80f79420c41fb044bb382b
Michael Krueger <michael.krueger@europace.de>
2019-06-17 21:34:12 +0200

    erster Test

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Mike!'
+
+
+@app.route('/goodbye')
+def goodbye():
+    return 'Goodbye Mike'
